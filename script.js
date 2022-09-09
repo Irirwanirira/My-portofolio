a81bbd73acc3d412e31c9a0ba3f7943d2b8e5c5a
Irirwanirira <irijoseph41@gmail.com>
2022-09-09 17:03:18 +0200

    Correct linter errors

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
 hamburger.addEventListener('click', () => {
-
-
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 })
